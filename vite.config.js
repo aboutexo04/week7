5422fc97c3883c682a81465eded6aac2211f385b
Seoyeon Mun <seoyeonmun@Seoyeons-MacBook-Pro.local>
2025-04-05 13:35:31 +0900

    Update vite config with rollup options

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
+  base: '/week5_scratch/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      external: ['/week5_scratch/assets/index-ad4531cb.js']
+    }
+  },
   plugins: [react()]
 })
