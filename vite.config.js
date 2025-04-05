7dddbd4630d3880596250569779b1ebced121714
Seoyeon Mun <seoyeonmun@Seoyeons-MacBook-Pro.local>
2025-04-05 16:35:03 +0900

    finished the project

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
-  build: {
-    outDir: '.'
-  },
+  base: '/week6_scratch/',
   plugins: [react()]
 })
