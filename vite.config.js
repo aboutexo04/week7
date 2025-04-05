080142532d54838226a377b7759159f5a2983c72
Seoyeon Mun <seoyeonmun@Seoyeons-MacBook-Pro.local>
2025-04-05 16:07:04 +0900

    Update base URL for week6_scratch

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/week5_scratch_1/',
+  base: '/week6_scratch/',
   build: {
     outDir: 'dist'
   },
