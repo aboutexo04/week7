c363eb5c8f36e58fa162562ed698c0e9186582a3
Seoyeon Mun <seoyeonmun@Seoyeons-MacBook-Pro.local>
2025-04-05 13:22:37 +0900

    Clean up deployment files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Week4_Scratch/',
+  base: '/week5_scratch/',
   plugins: [react()]
 })
