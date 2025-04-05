c41e0609169e5fe7f5e9c56cdce31ba3fffc799b
Seoyeon Mun <seoyeonmun@Seoyeons-MacBook-Pro.local>
2025-04-05 15:00:59 +0900

    fix working build and deploy setup

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/week5_scratch/',
+  base: '/week5_scratch_1/',
   build: {
     outDir: 'dist',
     rollupOptions: {
-      external: ['/week5_scratch/assets/index-ef820beb.js']
+      external: ['/week5_scratch_1/assets/index-ef820beb.js']
     }
   },
   plugins: [react()]
